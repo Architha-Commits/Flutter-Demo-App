e30dfcda1b49f472d6a1f17ab6d0d45587c26534
MDJAY <jayachandranmd.contact@gmail.com>
2024-08-22 16:00:35 +0530

    Initializations for hive and firebase

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:master_go_replication/firebase_options.dart';
+import 'package:master_go_replication/onboard/onboard_screens.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Hive.initFlutter();
+  await Hive.openBox<String>('notesBox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Ubuntu"),
+      home: const OnboardScreens(),
+    );
+  }
+}
